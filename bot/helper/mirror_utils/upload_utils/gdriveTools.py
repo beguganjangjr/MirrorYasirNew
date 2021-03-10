a30a77fc76eaa793f705d5e39144586221865cde
yasirarism <55983182+yasirarism@users.noreply.github.com>
2021-03-10 10:51:49 +0700

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -325,21 +325,21 @@ class GoogleDriveHelper:
                 buttons = button_build.ButtonMaker()
                 if SHORTENER is not None and SHORTENER_API is not None:
                     surl = requests.get('https://{}/api?api={}&url={}&format=text'.format(SHORTENER, SHORTENER_API, durl)).text
-                    buttons.buildbutton("⚡GDrive Link⚡", surl)
+                    buttons.buildbutton("💾⚡GDrive Link⚡💾", surl)
                 else:
-                    buttons.buildbutton("⚡GDrive Link⚡", durl)
+                    buttons.buildbutton("💾⚡GDrive Link⚡💾", durl)
                 if INDEX_URL is not None:
                     url = requests.utils.requote_uri(f'{INDEX_URL}/{meta.get("name")}/')
                     if SHORTENER is not None and SHORTENER_API is not None:
                         siurl = requests.get('https://{}/api?api={}&url={}&format=text'.format(SHORTENER, SHORTENER_API, url)).text
-                        buttons.buildbutton("💥Index Link💥", siurl)
+                        buttons.buildbutton("🚀 Index Link 🚀", siurl)
                     else:
-                        buttons.buildbutton("💥Index Link💥", url)
+                        buttons.buildbutton("🚀 Index Link 🚀", url)
                 if BUTTON_THREE_NAME is not None and BUTTON_THREE_URL is not None:
                     buttons.buildbutton(f"{BUTTON_THREE_NAME}", f"{BUTTON_THREE_URL}")
                 if BUTTON_FOUR_NAME is not None and BUTTON_FOUR_URL is not None:
                     url = requests.utils.requote_uri(f'{INDEX_URL}/{meta.get("name")}?a=view')
-                    buttons.buildbutton("Stream Link", url)
+                    buttons.buildbutton("📺 Stream Link 📺", url)
                 if BUTTON_FIVE_NAME is not None and BUTTON_FIVE_URL is not None:
                     buttons.buildbutton(f"{BUTTON_FIVE_NAME}", f"{BUTTON_FIVE_URL}")
             else:
@@ -349,9 +349,9 @@ class GoogleDriveHelper:
                 buttons = button_build.ButtonMaker()
                 if SHORTENER is not None and SHORTENER_API is not None:
                     surl = requests.get('https://{}/api?api={}&url={}&format=text'.format(SHORTENER, SHORTENER_API, durl)).text
-                    buttons.buildbutton("⚡GDrive Link⚡", surl)
+                    buttons.buildbutton("💾⚡GDrive Link⚡💾", surl)
                 else:
-                    buttons.buildbutton("⚡GDrive Link⚡", durl)
+                    buttons.buildbutton("💾⚡GDrive Link⚡💾", durl)
                 try:
                     msg += f'\n<b>📦 Ukuran : </b><code>{get_readable_file_size(int(meta.get("size")))}</code>'
                 except TypeError:
@@ -360,14 +360,14 @@ class GoogleDriveHelper:
                     url = requests.utils.requote_uri(f'{INDEX_URL}/{file.get("name")}')
                     if SHORTENER is not None and SHORTENER_API is not None:
                         siurl = requests.get('https://{}/api?api={}&url={}&format=text'.format(SHORTENER, SHORTENER_API, url)).text
-                        buttons.buildbutton("💥Index Link💥", siurl)
+                        buttons.buildbutton("🚀 Index Link 🚀", siurl)
                     else:
-                        buttons.buildbutton("💥Index Link💥", url)
+                        buttons.buildbutton("🚀 Index Link 🚀", url)
                 if BUTTON_THREE_NAME is not None and BUTTON_THREE_URL is not None:
                     buttons.buildbutton(f"{BUTTON_THREE_NAME}", f"{BUTTON_THREE_URL}")
                 if BUTTON_FOUR_NAME is not None and BUTTON_FOUR_URL is not None:
                     url = requests.utils.requote_uri(f'{INDEX_URL}/{meta.get("name")}?a=view')
-                    buttons.buildbutton("Stream Link", url)
+                    buttons.buildbutton("📺 Stream Link 📺", url)
                 if BUTTON_FIVE_NAME is not None and BUTTON_FIVE_URL is not None:
                     buttons.buildbutton(f"{BUTTON_FIVE_NAME}", f"{BUTTON_FIVE_URL}")
         except Exception as err:
