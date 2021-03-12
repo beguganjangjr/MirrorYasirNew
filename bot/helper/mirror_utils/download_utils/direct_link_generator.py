e51982aa3d6fad0b9ebcad8e46da6356ca24f293
yasirarism <55983182+yasirarism@users.noreply.github.com>
2021-03-12 09:22:17 +0700

    Update direct_link_generator.py

--- bot/helper/mirror_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_utils/download_utils/direct_link_generator.py
@@ -95,8 +95,13 @@ def files_im(url: str) -> str:
         link = re.findall(r'\bhttps?://.*files\.im\S+', url)[0]
     except IndexError:
         raise DirectDownloadLinkException("`No Files.im links found`\n")
-    page = BeautifulSoup(requests.get(link).content, 'lxml')
-    dl_url = page.find('a', {'class': 'btn btn-dow'})['href']
+    reqs=requests.get(link)
+    bss=BeautifulSoup(reqs.text,'html.parser')
+    op=bss.find('input',{'name':'op'})['value']
+    id=bss.find('input',{'name':'id'})['value']
+    rep=requests.post(link,data={'op':op,'id':id})
+    bss2=BeautifulSoup(rep.text,'html.parser')
+    dl_url=bss2.find('a',{'class':'btn btn-dow'})['href']
     return dl_url
 
 def yandex_disk(url: str) -> str:
